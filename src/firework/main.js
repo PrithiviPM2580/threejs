380980e83409ba5b73e830a6f4561339577216cb
Prithivi <prithivi@users.noreply.github.com>
2026-02-24 11:35:00 +0545

    feat: enhance firework simulation with sky effects and dynamic twinkling

--- src/firework/main.js
+++ src/firework/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Sky } from "three/addons/objects/Sky.js";
 import vertexShader from "./shader/vertex.glsl";
 import fragmentShader from "./shader/fragment.glsl";
 import GUI from "lil-gui";
@@ -67,6 +68,16 @@ const guiParams = {
   radius: 1,
   sphereCount: 100,
   color: "#8affff",
+  turbidity: 10,
+  rayleigh: 3,
+  mieCoefficient: 0.005,
+  mieDirectionalG: 0.95,
+  elevation: -2.2,
+  azimuth: 180,
+  exposure: renderer.toneMappingExposure,
+  cloudCoverage: 0.4,
+  cloudDensity: 0.4,
+  cloudElevation: 0.5,
 };
 
 gui.add(guiParams, "size", 0, 1, 0.01).onChange((value) => {
@@ -122,6 +133,7 @@ gui.addColor(guiParams, "color").onChange((value) => {
 const createFirework = (count, position, size, texture, radius, color) => {
   const positionArray = new Float32Array(count * 3);
   const sizeArray = new Float32Array(count);
+  const timeMultiplier = new Float32Array(count);
 
   for (let i = 0; i < count; i++) {
     const i3 = i * 3;
@@ -140,6 +152,8 @@ const createFirework = (count, position, size, texture, radius, color) => {
     positionArray[i3 + 2] = position.z;
 
     sizeArray[i] = Math.random();
+
+    timeMultiplier[i] = 1 + Math.random();
   }
 
   const geometry = new THREE.BufferGeometry();
@@ -148,6 +162,10 @@ const createFirework = (count, position, size, texture, radius, color) => {
     new THREE.BufferAttribute(positionArray, 3),
   );
   geometry.setAttribute("aSize", new THREE.BufferAttribute(sizeArray, 1));
+  geometry.setAttribute(
+    "aTimeMultiplier",
+    new THREE.BufferAttribute(timeMultiplier, 1),
+  );
 
   texture.flipY = false;
 
@@ -159,20 +177,24 @@ const createFirework = (count, position, size, texture, radius, color) => {
       uResolution: new THREE.Uniform(sizes.resolution),
       uTexture: new THREE.Uniform(texture),
       uColor: new THREE.Uniform(color),
-      uprogress: new THREE.Uniform(0),
+      uProgress: new THREE.Uniform(0),
     },
     transparent: true,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
   });
 
-  const points = new THREE.Points(geometry, material);
-  points.position.copy(position);
-  scene.add(points);
+  const firework = new THREE.Points(geometry, material);
+  firework.position.copy(position);
+  scene.add(firework);
 
-  function destroyFirework() {}
+  function destroyFirework() {
+    scene.remove(firework);
+    geometry.dispose();
+    material.dispose();
+  }
 
-  gsap.to(material.uniforms.uprogress, {
+  gsap.to(material.uniforms.uProgress, {
     value: 1,
     duration: 3,
     ease: "linear",
@@ -180,14 +202,77 @@ const createFirework = (count, position, size, texture, radius, color) => {
   });
 };
 
-createFirework(
-  guiParams.sphereCount, // count
-  new THREE.Vector3(), // position
-  guiParams.size, // size
-  textures[guiParams.imageIndex], // texture
-  guiParams.radius, // radius
-  new THREE.Color(guiParams.color), // color
-);
+const createRandomFirework = () => {
+  const count = Math.round(400 + Math.random() * 1000);
+  const position = new THREE.Vector3(
+    (Math.random() - 0.5) * 2,
+    Math.random(),
+    (Math.random() - 0.5) * 2,
+  );
+
+  const size = 0.1 + Math.random() * 0.1;
+  const texture = textures[Math.floor(Math.random() * textures.length)];
+  const radius = 0.5 + Math.random();
+  const color = new THREE.Color();
+  color.setHSL(Math.random(), 1, 0.7);
+
+  createFirework(count, position, size, texture, radius, color);
+};
+
+createRandomFirework();
+
+// Add click event listener to create fireworks on click
+window.addEventListener("click", createRandomFirework);
+
+//Sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+
+/// GUI
+
+function guiChanged() {
+  const uniforms = sky.material.uniforms;
+  uniforms["turbidity"].value = guiParams.turbidity;
+  uniforms["rayleigh"].value = guiParams.rayleigh;
+  uniforms["mieCoefficient"].value = guiParams.mieCoefficient;
+  uniforms["mieDirectionalG"].value = guiParams.mieDirectionalG;
+
+  const phi = THREE.MathUtils.degToRad(90 - guiParams.elevation);
+  const theta = THREE.MathUtils.degToRad(guiParams.azimuth);
+
+  sun.setFromSphericalCoords(1, phi, theta);
+
+  uniforms["sunPosition"].value.copy(sun);
+
+  renderer.toneMappingExposure = guiParams.exposure;
+}
+
+gui.add(guiParams, "turbidity", 0.0, 20.0, 0.1).onChange(guiChanged);
+gui.add(guiParams, "rayleigh", 0.0, 4, 0.001).onChange(guiChanged);
+gui.add(guiParams, "mieCoefficient", 0.0, 0.1, 0.001).onChange(guiChanged);
+gui.add(guiParams, "mieDirectionalG", 0.0, 1, 0.001).onChange(guiChanged);
+gui.add(guiParams, "elevation", 0, 90, 0.1).onChange(guiChanged);
+gui.add(guiParams, "azimuth", -180, 180, 0.1).onChange(guiChanged);
+gui.add(guiParams, "exposure", 0, 1, 0.0001).onChange(guiChanged);
+
+const folderClouds = gui.addFolder("Clouds");
+folderClouds
+  .add(guiParams, "cloudCoverage", 0, 1, 0.01)
+  .name("coverage")
+  .onChange(guiChanged);
+folderClouds
+  .add(guiParams, "cloudDensity", 0, 1, 0.01)
+  .name("density")
+  .onChange(guiChanged);
+folderClouds
+  .add(guiParams, "cloudElevation", 0, 1, 0.01)
+  .name("elevation")
+  .onChange(guiChanged);
+
+guiChanged();
 
 // Handle window resize
 window.addEventListener("resize", () => {
--- src/firework/shader/vertex.glsl
+++ src/firework/shader/vertex.glsl
@@ -3,20 +3,56 @@ varying vec2 vUv;
 
 uniform float uSize;
 uniform vec2 uResolution;
+uniform float uProgress;
 
 attribute float aSize;
+attribute float aTimeMultiplier;
+
+float remap(float value, float min1, float max1, float min2, float max2) {
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
 
 void main() {
-   vec4 modelPosition= modelMatrix * vec4(position, 1.0);
-   
+
+   float progress= uProgress * aTimeMultiplier;
+   vec3 newPosition = position;
+
+   //Exploding
+   float exploadingProgress= remap(progress, 0.0, 0.1, 0.0, 1.0);
+   exploadingProgress = clamp(exploadingProgress, 0.0, 1.0);
+   exploadingProgress = 1.0 - pow(1.0 - exploadingProgress, 3.0);
+   newPosition *= exploadingProgress;
+
+   //Falling
+   float fallingProgress= remap(progress, 0.1, 1.0, 0.0, 1.0);
+   fallingProgress = clamp(fallingProgress, 0.0, 1.0);
+   fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
+   newPosition.y -= fallingProgress * 0.2;
+
+   //Scaling
+   float sizeOpeningProgress= remap(progress, 0.0, 0.125, 0.0, 1.0);
+   float sizeClosingProgress= remap(progress, 0.125, 1.0, 1.0, 0.0);
+   float sizeProgress= min(sizeOpeningProgress, sizeClosingProgress);
+   sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
+   //Twinkling
+   float twinklingProgress= remap(progress, 0.2, 0.8, 0.0, 1.0);
+   twinklingProgress = clamp(twinklingProgress, 0.0, 1.0);
+   float sizeTwinkle= sin(twinklingProgress * 20.0) * 0.5 + 0.5;
+   sizeTwinkle = 1.0 -sizeTwinkle * twinklingProgress; // fade in twinkle
 
 
+   vec4 modelPosition= modelMatrix * vec4(newPosition, 1.0);
    vec4 viewPosition = viewMatrix * modelPosition;
    vec4 projectedPosition = projectionMatrix * viewPosition;
    gl_Position = projectedPosition;
 
-   gl_PointSize= uSize * uResolution.y * aSize;
+   gl_PointSize= uSize * uResolution.y * aSize * sizeProgress * sizeTwinkle;
    gl_PointSize *= (1.0 / - viewPosition.z); // perspective correction
    vUv = uv;
+
+   if(gl_PointSize < 1.0) {
+     gl_Position = vec4(9999.0);
+   }
  
 }
