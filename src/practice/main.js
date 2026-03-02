9defc43618d111f8922991a4e1eb0276733772a0
Prithivi <prithivi@users.noreply.github.com>
2026-03-02 12:09:40 +0545

    feat: add ocean video background and update shaders for video texture handling

--- src/practice/main.js
+++ src/practice/main.js
@@ -5,12 +5,15 @@ import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import "./style.css";
 import fragmentShader from "./shader/fragment.glsl";
 import vertexShader from "./shader/vertex.glsl";
+const imageLink =
+  "https://images.unsplash.com/photo-1500881263786-ad74c00b9e60?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 // GUI setup
 const gui = new GUI();
 
 const parameters = {
   size: 1,
+  mix: 0.5,
 };
 
 // Scene setup
@@ -20,22 +23,48 @@ scene.background = new THREE.Color(0x000000);
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRation: Math.min(window.devicePixelRatio, 2),
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 };
 
+const textureLoader = new THREE.TextureLoader();
+const textureResolution = new THREE.Vector2(1, 1);
+const videoResolution = new THREE.Vector2(16, 9);
+const texture = textureLoader.load(imageLink, (loadedTexture) => {
+  textureResolution.set(loadedTexture.image.width, loadedTexture.image.height);
+});
+
+const videoDOM = document.getElementById("video-bg");
+videoDOM.crossOrigin = "anonymous";
+const video = new THREE.VideoTexture(videoDOM);
+videoDOM.addEventListener("loadedmetadata", () => {
+  videoResolution.set(videoDOM.videoWidth, videoDOM.videoHeight);
+});
+
 // Camera setup
-const camera = new THREE.PerspectiveCamera(
-  70,
-  sizes.width / sizes.height,
+// const camera = new THREE.PerspectiveCamera(
+//   70,
+//   sizes.width / sizes.height,
+//   0.1,
+//   1000,
+// );
+// camera.position.z = 4;
+const frustumSize = 1;
+const aspect = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  (-frustumSize * aspect) / 2,
+  (frustumSize * aspect) / 2,
+  frustumSize / 2,
+  -frustumSize / 2,
   0.1,
   1000,
 );
-camera.position.z = 200;
+camera.position.z = 1;
+scene.add(camera);
 
 // Renderer setup
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(sizes.pixelRation);
+renderer.setPixelRatio(sizes.pixelRatio);
 document.body.appendChild(renderer.domElement);
 
 // Orbit Controls
@@ -47,16 +76,53 @@ controls.dampingFactor = 0.05;
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
 
-//Geometry
+//Geometry and Material
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    uTime: new THREE.Uniform(0),
+    uResolution: new THREE.Uniform(
+      new THREE.Vector2(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio,
+      ),
+    ),
+    uTextureResolution: new THREE.Uniform(textureResolution),
+    uVideoResolution: new THREE.Uniform(videoResolution),
+    uVideo: new THREE.Uniform(video),
+    uTexture: new THREE.Uniform(texture),
+    uMix: new THREE.Uniform(parameters.mix),
+  },
+});
+
+gui.add(parameters, "mix", 0, 1, 0.001).onChange((value) => {
+  material.uniforms.uMix.value = value;
+});
+
+const plane = new THREE.Mesh(geometry, material);
+plane.scale.set(frustumSize * aspect, frustumSize, 1);
+scene.add(plane);
 
 // Handle window resize
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
-  camera.aspect = sizes.width / sizes.height;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+  const resizedAspect = sizes.width / sizes.height;
+  camera.left = (-frustumSize * resizedAspect) / 2;
+  camera.right = (frustumSize * resizedAspect) / 2;
+  camera.top = frustumSize / 2;
+  camera.bottom = -frustumSize / 2;
+  plane.scale.set(frustumSize * resizedAspect, frustumSize, 1);
+  material.uniforms.uResolution.value.set(
+    sizes.width * sizes.pixelRatio,
+    sizes.height * sizes.pixelRatio,
+  );
   camera.updateProjectionMatrix();
   renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(sizes.pixelRation);
+  renderer.setPixelRatio(sizes.pixelRatio);
 });
 
 const clock = new THREE.Clock();
--- src/practice/shader/fragment.glsl
+++ src/practice/shader/fragment.glsl
@@ -1,7 +1,45 @@
 
+varying vec2 vUv;
 
+uniform sampler2D uTexture;
+uniform sampler2D uVideo;
+uniform vec2 uResolution;
+uniform vec2 uTextureResolution;
+uniform vec2 uVideoResolution;
+uniform float uMix;
+
+vec2 coverUv(vec2 uv, vec2 outputResolution, vec2 mediaResolution) {
+    float outputAspect = outputResolution.x / outputResolution.y;
+    float mediaAspect = mediaResolution.x / mediaResolution.y;
+
+    vec2 newUv = uv;
+
+    if (outputAspect > mediaAspect) {
+        float scale = mediaAspect / outputAspect;
+        newUv.y = uv.y * scale + (1.0 - scale) * 0.5;
+    } else {
+        float scale = outputAspect / mediaAspect;
+        newUv.x = uv.x * scale + (1.0 - scale) * 0.5;
+    }
+
+    return newUv;
+}
 
 void main(){
+
+    //Remap Resolution
+    vec2 textureUv = coverUv(vUv, uResolution, uTextureResolution);
+    vec2 videoUv = coverUv(vUv, uResolution, uVideoResolution);
+
+    //Distance from center
+    vec2 circleUv= vUv - vec2(0.5);
+    float distanceFromCenter= length(circleUv);
     
-    gl_FragColor = vec4(0.728,0.592,0.677, 1.0); 
+
+    vec4 textureColor = texture2D(uTexture, textureUv);
+    vec4 videoColor = texture2D(uVideo, videoUv);
+    gl_FragColor = vec4(distanceFromCenter);
+    // gl_FragColor = vec4(vUv,1.0, 1.0); 
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
 }
